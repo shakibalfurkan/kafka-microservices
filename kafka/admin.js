6cb3e300a2eaa0adccdf43f6ef27e3fc516e1cdf
shakibalfurkan <shakibalfurkan@gmail.com>
2025-12-07 18:42:04 +0600

    multiple broker added

--- kafka/admin.js
+++ kafka/admin.js
@@ -2,7 +2,7 @@ import { Kafka } from "kafkajs";
 
 const kafka = new Kafka({
   clientId: "kafka-service",
-  brokers: ["localhost:9094"],
+  brokers: ["localhost:9094", "localhost:9095", "localhost:9096"],
 });
 
 const admin = kafka.admin();
